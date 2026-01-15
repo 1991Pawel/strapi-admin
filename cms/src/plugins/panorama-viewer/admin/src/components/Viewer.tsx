92dd26704aea7e07b301ee0603fa050500373525
Paweł Leśnik <pawel.lesnik@tdy.pl>
2026-01-15 09:29:01 +0100

    add useMemo to panorama

--- cms/src/plugins/panorama-viewer/admin/src/components/Viewer.tsx
+++ cms/src/plugins/panorama-viewer/admin/src/components/Viewer.tsx
@@ -2,6 +2,7 @@ import { Box } from '@strapi/design-system';
 import { PanoramaCanvas } from './PanoramaCanvas';
 import { Canvas } from '@react-three/fiber';
 import { type EditorState, type StateSetter } from '../types';
+import { useMemo } from 'react';
 import { useTexture } from '@react-three/drei';
 
 type ViewerProps = {
@@ -13,13 +14,16 @@ const Viewer = ({ editorState, setEditorState }: ViewerProps) => {
     return <Box>No panorama loaded</Box>;
   }
 
-  const file = editorState.panoramas[0];
-  const src = URL.createObjectURL(file);
+  const fileSrc = useMemo(() => {
+    const file = editorState.panoramas[0];
+    const src = URL.createObjectURL(file);
+    return src;
+  }, [editorState.panoramas]);
 
   return (
     <Box padding={6} style={{ width: '100%', height: '100%' }}>
       <Canvas camera={{ fov: 75, position: [0, 0, 0] }}>
-        <PanoramaCanvas src={src} editorState={editorState} setEditorState={setEditorState} />
+        <PanoramaCanvas src={fileSrc} editorState={editorState} setEditorState={setEditorState} />
       </Canvas>
     </Box>
   );
